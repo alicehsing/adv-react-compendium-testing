c5a721769b16993e519a5c55bdc0622707267a6d
Alice Hsing <alicehsing@gmail.com>
2022-04-26 15:26:33 -0700

    set up list and detail routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import { Route, Switch, Link, BrowserRouter as Router } from 'react-router-dom';
+import PokemonList from './views/Pokemon/List';
+import PokemonDetail from './views/Pokemon/Detail';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Switch>
+      <Route path="/pokemon/:id">
+        <PokemonDetail />
+      </Route>
+      <Route path="/pokemon">
+        <PokemonList />
+      </Route>
+      <Route path="/">
+        <p>Home</p>
+        <Link to="/pokemon">View Pokemon List</Link>
+      </Route>
+    </Switch>
+  );
+  
 }

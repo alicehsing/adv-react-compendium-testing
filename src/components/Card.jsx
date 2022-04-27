88a7ff510b7be0e70c27961bf4734aabf0c72eac
Alice Hsing <alicehsing@gmail.com>
2022-04-26 18:50:32 -0700

    added PokemonCard component and successfully rendered each pokemon object with some styling

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import styles from '../App.css';
+export default function PokemonCard({ name, image, type, defenseLevel }) {
+  return (
+    <div className={styles.card}>
+      <h3>{name.toUpperCase()}</h3>
+      <p>Type: {type}</p>
+      <p>Defense Level: {defenseLevel}</p>
+      <img alt="image" src={image} />
+    </div>
+  );
+}
--- src/views/Pokemon/List.jsx
+++ src/views/Pokemon/List.jsx
@@ -1,12 +1,20 @@
 import { useEffect, useState } from 'react';
+import styles from '../../App.css';
+import PokemonCard from '../../components/Card';
 
 export default function PokemonList() {
   // set state
-  const [pokemons, setPokemons] = useState([]); // store all pokemons
+  const [pokemons, setPokemons] = useState([]);
   const [loading, setLoading] = useState(true);
   const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]); //stores a filtered subset of pokemons
 
-  // fetching pokemon API, create an object with the info I want to access to
+  // if there is a search of any length; !! converting value into boolean
+  const isSearching = !!search.length;
+  // if there is a search, show the results, otherwise show all pokemons
+  const pokeList = isSearching ? results : pokemons;
+
+  // useEffect to fetch pokemon API, create an object with the info I want to access to
   useEffect(() => {
     const getPokemons = async () => {
       const res = await fetch(
@@ -25,7 +33,16 @@ export default function PokemonList() {
     getPokemons();
   }, []);
 
-  // /search bar
+  // function for handling search input, will also filter by name, be case sensitive and remove extra space
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+    const searchResults = pokemons.filter((pokemon) =>
+      pokemon.name
+        .toLowerCase()
+        .includes(event.target.value.toLowerCase().trim())
+    );
+    setResults(searchResults);
+  };
 
   return (
     <>
@@ -34,11 +51,27 @@ export default function PokemonList() {
       ) : (
         <>
           <input
-            placeholder="Seach a Pokemon"
+            placeholder="Search by name"
             type="text"
             value={search}
-            onChange={(e) => setSearch(e.target.value)}
+            onChange={() => {
+              handleSearch;
+            }}
           />
+          <h2>List of Pokemons</h2>
+
+          <div className={styles.list}>
+            {pokeList.map((pokemon) => {
+              return (
+                <PokemonCard
+                  name={pokemon.name}
+                  image={pokemon.image}
+                  type={pokemon.type}
+                  defenseLevel={pokemon.defenseLevel}
+                />
+              );
+            })}
+          </div>
         </>
       )}
     </>

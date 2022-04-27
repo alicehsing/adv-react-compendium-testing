dfe618d033ed0968a961888c06dff68f73c30213
Alice Hsing <alicehsing@gmail.com>
2022-04-26 23:55:54 -0700

    wrote behavioral test, work in progress...

--- src/views/Pokemon/List.test.jsx
+++ src/views/Pokemon/List.test.jsx
@@ -17,3 +17,29 @@ describe('Pokemon list - component test', () => {
     // await screen.findAllByAltText('pokemon');
   });
 });
+
+describe('Pokemon list - behavioral test', () => {
+  it('should render Chansey when Chansey is searched for', async () => {
+    render(
+      <MemoryRouter>
+        <List />
+      </MemoryRouter>
+    );
+
+    // find the search/filter input box
+    const search = screen.getByPlaceholderText('Search by name');
+
+    // type the word "Chansey" into our search input
+    userEvent.type(search, 'Chansey');
+
+    // return waitFor(() => {
+    //   // check that only "Chansey" renders
+    // const result = await screen.findByText('Chansey');
+    // expect(result.textContent).toEqual('Chansey');
+    const result = await screen.findAllByRole('heading');
+    expect(result.length).toEqual(1);
+    expect(result[0].textContent).toEqual('Chansey');
+    // });
+    // await screen.findAllByRole('heading', { name: 'Chansey ' });
+  });
+});
